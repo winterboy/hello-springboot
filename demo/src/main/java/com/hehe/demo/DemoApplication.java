9b0a60f2246b37afd97701e2c2ad9ba45a9daa32
chung.nguyen <chung.nguyen@terralogic.com>
2026-02-03 17:06:19 +0700

    add sysout

--- demo/src/main/java/com/hehe/demo/DemoApplication.java
+++ demo/src/main/java/com/hehe/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 //		SpringApplication.run(DemoApplication.class, args);
 		SpringApplication springApplication = new SpringApplication(DemoApplication.class);
 		springApplication.run(args);
+		System.out.println("hehe");
 	}
 
 }
